51e9dd2dd60e823e1f49bf2fd8e550dd6f017d9d
Mike Magnani <mikemagnani5@gmail.com>
2016-08-14 11:49:00 -0400

    created table

--- server.js
+++ server.js
@@ -1,5 +1,53 @@
 var express = require('express');
-var bodyParser = require('bodyParser');
+var bodyParser = require('body-parser');
 var handlebars = require('express-handlebars');
 
+
+var Posts = require('./models')['Posts'];
+Posts.sync();
+
+
 var app = express();
+
+app.use(express.static(__dirname,  + '/public'));
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.engine('handlebars', handlebars({
+  defualtLayout: 'main'
+}));
+
+app.set('view engine', 'handlebars');
+
+//home page
+app.get('/', function(req, res){
+    
+  Posts.findAll({}).then(function(result){
+    console.log(result);
+    return res.render('index', {
+      post: result
+    });
+
+  });
+    
+
+})
+
+// form page
+app.get('/new-post', function(req, res){
+    res.render('new');
+})
+
+app.get('/posts/:id', function(req, res){
+    res.render('post');
+})
+
+
+
+
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+  console.log('connection running')
+});
